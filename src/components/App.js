9c2a6e43e48d18446b68fc2f37dfdcef49899043
pevargasg <wearetamo@gmail.com>
2019-03-14 15:30:01 -0700

    search bar implemented

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar />
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div> SearchBar</div>;
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
