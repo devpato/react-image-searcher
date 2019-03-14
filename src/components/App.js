73eaadba49ff66e4cbfa172ffb7c0299375e79c0
pevargasg <wearetamo@gmail.com>
2019-03-14 16:45:50 -0700

    installed axios as a dependency to our project

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import SearchBar from "./SearchBar";
+import axios from "axios";
 
 class App extends React.Component {
   onSearchSubmit(term) {
