8c9693ce57ec5bb1386f6af2a076ef0524818ba8
pevargasg <wearetamo@gmail.com>
2019-03-14 16:57:31 -0700

    using async await to get data

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import SearchBar from "./SearchBar";
 import axios from "axios";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term
       },
@@ -13,6 +13,10 @@ class App extends React.Component {
           "Client-ID cbefb7b2ce25d1fdbf242be7fbf7f3cc482ee5628a19b4b6e4ad0b062892907f"
       }
     });
+    // .then(res => {
+    //   console.log(res.data.results);
+    // });
+    console.log(response.data.results);
   }
   render() {
     return (
