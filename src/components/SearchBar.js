2e6a645b54c9b4deec9cb060658c9c99fb268943
pevargasg <wearetamo@gmail.com>
2019-03-14 16:10:45 -0700

    fixed issue with state undifined

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,23 +5,28 @@ class SearchBar extends React.Component {
     term: "Type a search term"
   };
 
-  onInputClick() {
+  onInputClick = () => {
     if (this.state.term === "Type a search term") {
       this.setState({ term: "" });
     }
-  }
+  };
+
+  onSearch = e => {
+    e.preventDefault();
+    console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onSearch}>
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
               onChange={e => this.setState({ term: e.target.value })}
-              onClick={() => this.onInputClick()}
+              onClick={this.onInputClick}
             />
           </div>
         </form>
