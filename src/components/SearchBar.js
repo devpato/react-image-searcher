44a8a7f10343c49854d8aa3574aa0c26624e4c98
pevargasg <wearetamo@gmail.com>
2019-03-14 15:40:36 -0700

    chaing out searchnbar from uncontrolled to controlled

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
