4cc1f3e2fd12ee1e5c92519b9503eed6342bf11d
pevargasg <wearetamo@gmail.com>
2019-03-14 17:03:17 -0700

    code clean up

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import SearchBar from "./SearchBar";
 import axios from "axios";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term
@@ -16,12 +20,13 @@ class App extends React.Component {
     // .then(res => {
     //   console.log(res.data.results);
     // });
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
