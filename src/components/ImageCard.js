b21b9d5bb909b1cd93ed1b7f0e07154085c39df6
pevargasg <wearetamo@gmail.com>
2019-03-15 21:03:21 -0700

    grid system done with react

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const spans = Math.ceil(this.imageRef.current.clientHeight / 10) + 1;
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
