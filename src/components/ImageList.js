42f936073d2fc128bba17963ab089794c055080c
pevargasg <wearetamo@gmail.com>
2019-03-15 20:37:04 -0700

    cards for images created

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 const ImageList = props => {
+  let IMAGES;
   if (props.images.length > 0) {
-    return props.images.reduce(
-      (acc, val, index) =>
-        acc.concat(<img src={val.urls.regular} alt={val.id} key={index} />),
+    IMAGES = props.images.reduce(
+      (acc, val, index) => acc.concat(<ImageCard key={index} image={val} />),
       []
     );
+    return <div className="image-list">{IMAGES}</div>;
   } else {
     return null;
   }
